e83239ab39db0acdf330b5ede940c8e41acb7e29
igorrm19 <igormachado14@gmail.com>
2025-09-18 21:42:02 -0300

    2: 18/09

---
+++ src/app/components/form/form.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Horario } from '../svg/horario/horario';
+
+@Component({
+  selector: 'app-form',
+  imports: [Horario],
+  templateUrl: './form.html',
+  styleUrl: './form.scss'
+})
+export class Form {
+
+}
---
+++ src/app/components/svg/horario/horario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horario',
+  imports: [],
+  templateUrl: './horario.html',
+  styleUrl: './horario.scss'
+})
+export class Horario {
+
+}
